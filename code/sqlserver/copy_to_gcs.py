4d80a355e0440771576c6998c0e2eff7a4c44a97
afowles <aaron.fowles@dyson.com>
2018-11-21 08:20:03 +0000

    correct typo

--- code/sqlserver/copy_to_gcs.py
+++ code/sqlserver/copy_to_gcs.py
@@ -5,7 +5,7 @@ import logging
 if __name__ == '__main__':
 
     logging.basicConfig(format='%(levelname)s: %(asctime)s %(message)s', level=logging.INFO)
-    logger = logging.getLogger(__name__)
+    log = logging.getLogger(__name__)
 
     log.info('Starting process.')
 

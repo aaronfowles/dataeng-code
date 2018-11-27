ffdf21c3eff56745412ec930ab3849ac663f638a
afowles <aaron.fowles@dyson.com>
2018-11-27 09:05:52 +0000

    test PR process

---
+++ Makefile
@@ -0,0 +1,2 @@
+make_requirements:
+    pip freeze > requirements.txt
--- dataeng_code/sqlserver/copy_to_gcs.py
+++ dataeng_code/sqlserver/copy_to_gcs.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 
+from google.cloud import storage
+
 import logging
 
+class BucketIO:
 
-def do_something():
-    return 'something'
+    def __init__(self):
+        self.bucket_name = None
+        self.bucket = None
+
+    def get_bucket(self):
+        if self.bucket is None:
+            storage_client = storage.Client()
+            bucket = storage_client.get_bucket(self.bucket_name)
+        else:
+            bucket = self.bucket
+        return bucket
+
+
+    def get_filenames(self):
+        return [blob.name for blob in self.bucket.list_blobs()]
 
 
 if __name__ == '__main__':
@@ -13,5 +29,15 @@ if __name__ == '__main__':
     log = logging.getLogger(__name__)
 
     log.info('Starting process.')
+    bucket_io = BucketIO()
+    bucket_io.bucket_name = 'dev-cloud-composer-testing'
+    bucket_io.bucket = bucket_io.get_bucket()
+
+    blob = bucket_io.bucket.blob('test_upload')
+
+    blob.upload_from_string(
+        ', '.join(bucket_io.get_filenames()),
+        content_type='text'
+    )
 
     log.info('Finishing process.')
